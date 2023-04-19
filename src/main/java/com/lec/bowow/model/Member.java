9449869edd2840c1e1a2b548d27745a5a5f85fdc
Alipheese16th <shwoo1103@gmail.com>
2023-04-19 15:36:17 +0900

    ttttttttttt

---
+++ src/main/java/com/lec/bowow/model/Member.java
@@ -0,0 +1,17 @@
+package com.lec.bowow.model;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class Member {
+	private String memberId;
+	private String memberPw;
+	private String memberName;
+	private String memberEmail;
+	private String memberTel;
+	private String memberAddr;
+	private Date memberBirth;
+	private int memberPoint;
+}
